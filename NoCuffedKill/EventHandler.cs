3d99ca70d80ab856922873b3bc135599492a0a1f
Ben <benjamin.t.harrison24@gmail.com>
2021-11-02 00:03:14 +0000

    Add project files.

---
+++ NoCuffedKill/EventHandler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MEC;
+using Hints;
+using UnityEngine;
+using Exiled.Events.EventArgs;
+using Player = Exiled.API.Features.Player;
+
+namespace NoCuffedKill
+{
+    public class EventHandler
+    {
+        private static bool Reflect = NoCuffedKill.config.RelfectDamage;
+
+
+        public static void OnPlayerHurt(HurtingEventArgs ev)
+        {
+            Player A = ev.Attacker;
+            Player T = ev.Target;
+
+            if(ev.Target.IsCuffed)
+            {
+                switch(T.Team)
+                {
+                    case Team.CDP:
+
+                        switch(A.Team)
+                        {
+                            case Team.MTF:
+                                T.Health += ev.Amount;
+                                if (Reflect)
+                                {
+                                    A.Health -= ev.Amount;
+                                }
+                                break;
+
+                            case Team.RSC:
+                                T.Health += ev.Amount;
+                                if(Reflect)
+                                {
+                                    A.Health -= ev.Amount;
+                                }
+                                break;
+                        }
+                        break;
+
+                    case Team.RSC:
+
+                        switch(ev.Attacker.Team)
+                        {
+                            case Team.CDP:
+                                T.Health += ev.Amount;
+                                if (Reflect)
+                                {
+                                    A.Health -= ev.Amount;
+                                }
+                                break;
+
+                            case Team.CHI:
+                                T.Health += ev.Amount;
+                                if (Reflect)
+                                {
+                                    A.Health -= ev.Amount;
+                                }
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ NoCuffedKill/NoCuffedKill.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using MEC;
+using Server = Exiled.Events.Handlers.Server;
+using Player = Exiled.Events.Handlers.Player;
+
+namespace NoCuffedKill
+{
+    public class NoCuffedKill : Plugin<Config>
+    {
+        public static Config config;
+        public EventHandler EventHandler;
+        public override string Name { get; } = "NoCuffedKill";
+        public override string Author { get; } = "Thire";
+        public override Version Version { get; } = new Version(0, 1, 1);
+        public override Version RequiredExiledVersion { get; } = new Version(3, 3, 1);
+        public override string Prefix { get; } = "NCK";
+        public override PluginPriority Priority { get; } = PluginPriority.Medium;
+
+        internal static NoCuffedKill Instance;
+        public override void OnEnabled()
+        {
+            Log.Info("NoCuffedKill is currently Enabled, Thank you!");
+            Instance = this;
+            EventHandler = new EventHandler();
+            Player.Hurting += EventHandler.OnPlayerHurt;
+        }
+
+        public override void OnDisabled()
+        {
+            Player.Hurting -= EventHandler.OnPlayerHurt;
+        }
+    }
+}
